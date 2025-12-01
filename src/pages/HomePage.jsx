734178fac28bd0ff0c5f1ba92ebf38da52356cb7
LyTruongPhuc <phucly18605@gmail.com>
2025-12-02 00:08:00 +0700

    dashboard calm

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,45 @@
 import React from "react";
 import Layout from "../components/Layout";
 import "../styles/layout.css";
+import "../styles/HomePage.css";
 
 function HomePage() {
 
   return (
      <Layout>
-      <h1>Welcome to the Dashboard</h1>
+      <div className="home-page">
+        <h1>Welcome to the Recruitment and Training Management System</h1>
+      
+      <div className="home-page fade-slide">
+
+        {/* --- TAB HEADER --- */}
+        <div className="dashboard-tabs">
+          <div className="tab active">
+            CHỈ SỐ 
+          </div>
+          <div className="tab">
+            BIỂU ĐỒ 
+          </div>
+          <div className="tab">
+            THỐNG KÊ TĂNG TRƯỞNG 
+          </div>
+        </div>
+
+        {/* --- TITLE --- */}
+        <h2 className="dashboard-title">
+          Kết quả đào tạo 
+        </h2>
+
+        {/* --- FILTER BUTTONS --- */}
+        <div className="filter-buttons">
+          <button className="filter active">Theo tháng </button>
+          <button className="filter">Theo quý </button>
+          <button className="filter">Theo năm </button>
+          <button className="filter">Tất cả </button>
+        </div>
+
+      </div>
+      </div>
     </Layout>
   );
 }
